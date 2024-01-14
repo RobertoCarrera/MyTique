7a0f9526ec6b7e247dd4572aa940b81a3c525d14
RobertoCarrera <robertocarrera93@gmail.com>
2024-01-14 18:02:26 +0100

    Added routes to the routing. Working great

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SummaryComponent } from './components/summary/summary.component';
+import { OutcomesComponent } from './components/outcomes/outcomes.component';
+import { IncomesComponent } from './components/incomes/incomes.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',pathMatch: 'full', redirectTo: 'resumen'
+  },
+  {
+    path: 'resumen',
+    component: SummaryComponent
+  },
+  {
+    path: 'gastos',
+    component: OutcomesComponent
+  },
+  {
+    path: 'ahorros',
+    component: IncomesComponent
+  },
+  {
+    path: 'perfil',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
