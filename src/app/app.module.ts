029c9a906f27ea95b56fed768cbc5d8ad130b4f0
RobertoCarrera <robertocarrera93@gmail.com>
2024-01-14 17:57:10 +0100

    Created profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './utils/navbar/navbar.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { OutcomesComponent } from './components/outcomes/outcomes.component';
 import { IncomesComponent } from './components/incomes/incomes.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { IncomesComponent } from './components/incomes/incomes.component';
     NavbarComponent,
     SummaryComponent,
     OutcomesComponent,
-    IncomesComponent
+    IncomesComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
